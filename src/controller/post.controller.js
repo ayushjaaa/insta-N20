23f30560b7b2e845359cf8fdcbacb76cdf2086d7
Aysh Jaiswal <ayushjaiswal452@gmail.com>
2025-03-01 23:53:32 +0530

    5th commit

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,8 +1,9 @@
 
-import { generateCaptionFromImageBuffer } from "../services/ai.services"
+import { generateCaptionFromImageBuffer } from "../services/ai.services.js";
 export const createPost = async(req,res,next) =>{
-const  imageBuffer = req.file.buffer
+const  imageBuffer = req.file.buffer;
 const caption  = await generateCaptionFromImageBuffer(imageBuffer)
+// console.log("post chala")
 res.status(201).json({
     caption
 })
--- src/middelwares/post.moideeleware.js
+++ src/middelwares/post.moideeleware.js
@@ -1,8 +1,6 @@
 import multer from "multer";
 const storage = multer.memoryStorage()
-const upload = multer({storage:storage,
-    limits:{
-        fieldSize:1024 * 1024 * 5
-    }
-})
+const upload = multer({ storage: storage,
+ 
+ })
 export const handleFileUpload = upload.single('image')
