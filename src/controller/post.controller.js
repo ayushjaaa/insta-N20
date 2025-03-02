129c822228569196c48a683ce8e6e84cba438a13
Aysh Jaiswal <ayushjaiswal452@gmail.com>
2025-03-02 14:28:25 +0530

    see whaat response we are getting after sending data from server and read with multer to cloudinary

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,12 +1,12 @@
 
 import { generateCaptionFromImageBuffer } from "../services/ai.services.js";
-
+import { uploadFile } from "../services/cloud.storage.services.js";
 export const createPost = async(req,res,next) =>{
 const  imageBuffer = req.file.buffer;
-
-const caption  = await generateCaptionFromImageBuffer(imageBuffer)
-console.log("post chala")
+const filedata = await uploadFile(imageBuffer)
+// const caption  = await generateCaptionFromImageBuffer(imageBuffer)
+// console.log("post chala")
 res.status(201).json({
-caption
+    filedata
 })
 }
